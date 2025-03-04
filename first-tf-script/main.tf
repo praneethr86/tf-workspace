7579ad77ff855b24f278cacc73b7125410db16b0
Praneeth Patakota <praneeth001@gmail.com>
2025-03-04 23:42:57 +0530

    trying output

--- first-tf-script/main.tf
+++ first-tf-script/main.tf
@@ -8,3 +8,20 @@ resource local_file sample_resource2 {
     filename = "${path.module}/test2.txt"
     content  = "Hello, Terraform again!"
 }
+
+resource random_integer sample_resource3 {
+    min = 1
+    max = 100
+}
+
+output randomint1 {
+    value = random_integer.sample_resource3.result
+}
+
+resource "random_string" "name2" {
+  length = 15
+}
+
+output randomstr1 {
+    value = random_string.name2.result
+}
